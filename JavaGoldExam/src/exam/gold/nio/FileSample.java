38b0cd1d7c57cd27d065cc711e9223e885719d33
Irie HIronori <irie.h126@gmail.com>
2019-08-12 12:40:31 +0900

    意図せずパッケージ名がJavaライブラリとかぶってしまったので修正

---
+++ JavaGoldExam/src/exam/gold/nio/FileSample.java
@@ -0,0 +1,14 @@
+package exam.gold.nio;
+
+import java.io.File;
+
+public class FileSample {
+
+    public static void main(String[] args) {
+        File f1 = new File("resouces");
+
+        System.out.println(f1.getName());
+
+    }
+
+}
