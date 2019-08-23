c0e7749b75b37a0ba61cf21c8c1101e43faf11b3
Irie HIronori <irie.h126@gmail.com>
2019-08-23 13:24:53 +0900

    Stream APIと日付時刻APIのサンプル

--- JavaGoldExam/src/exam/gold/stream/StreamSample.java
+++ JavaGoldExam/src/exam/gold/stream/StreamSample.java
@@ -1,21 +1,43 @@
 package exam.gold.stream;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamSample {
 
     public static void main(String[] args) {
         boolean result;
 
-        result =  Arrays.asList(1,2,3,4,5,6,7,8,9,10).stream().allMatch((i)->i < 11);
-        System.out.println("Result:"+result);
+        result = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).stream().allMatch((i) -> i < 11);
+        System.out.println("Result:" + result);
 
-        result =  Arrays.asList(1,2,3,4,5,6,7,8,9,10).stream().allMatch((i)->i < 10);
-        System.out.println("Result:"+result);
+        result = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).stream().allMatch((i) -> i < 10);
+        System.out.println("Result:" + result);
 
-        int[] ia = IntStream.iterate(1, (i)->++i).limit(20).filter((i)->i%3 !=0).toArray();
-        System.out.println("Result:"+ Arrays.toString(ia));
+        int[] ia = IntStream.iterate(1, (i) -> ++i).limit(20).filter((i) -> i % 3 != 0).toArray();
+        System.out.println("Result:" + Arrays.toString(ia));
+
+
+
+        List<String> strList = Arrays.asList("abc", "de", "1", "fghi", "jkl", "z", "qryuoy");
+        Stream<String> stremString = strList.stream();
+        String result3 = stremString.filter(s -> s.length() >= 2)
+                .map(s -> s.length() + ":" + s)
+                .collect(Collectors.joining(","));
+        System.out.println(result3);
+
+        Map<Integer, String> map = new TreeMap<>();
+        map.put(1, "Alc");
+        map.put(2, "Buffer");
+        map.put(3, "Caloly");
+        map.forEach((k, v) -> System.out.println(k + ":" + v));
+
+        Stream.of("a", "b", "c", "d", "e", "f");
 
     }
 
---
+++ JavaGoldExam/src/exam/gold/time/TimeSample.java
@@ -0,0 +1,120 @@
+package exam.gold.time;
+
+import static java.lang.System.*;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+
+public class TimeSample {
+
+    public static void main(String[] args) {
+        DateTimeFormatter dtf1 = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+        DateTimeFormatter dtf2 = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        DateTimeFormatter dtf3 = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+        DateTimeFormatter dtf4 = DateTimeFormatter.ofLocalizedTime(FormatStyle.FULL);
+        DateTimeFormatter dtf5 = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+        DateTimeFormatter dtf6 = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL);
+        out.println(LocalTime.now());
+        //out.println(LocalTime.now().format(dtf4));
+        out.println(LocalTime.now().format(dtf5));
+        out.println(LocalDate.now());
+        out.println(LocalDate.now().format(dtf1));
+        out.println(LocalDate.now().format(dtf2));
+        out.println(LocalDateTime.now());
+        out.println(LocalDateTime.now().format(dtf3));
+        out.println(OffsetTime.now());
+        out.println(OffsetDateTime.now());
+        out.println("------------------");
+        LocalTime lt1 = LocalTime.of(1, 59, 5, 200);
+        LocalTime lt2 = LocalTime.parse("08:31:09");
+        //DateTimeParseException
+        //LocalTime lt2 = LocalTime.parse("8:31:9");
+        out.println(lt1);
+        out.println(lt2);
+
+        {
+            LocalDate ld1 = LocalDate.of(2020, 12, 6);
+            out.println(ld1);
+        }
+
+        {
+            LocalDate ld1 = LocalDate.now();
+            LocalDate ld2 =ld1.minusDays(2200);
+            out.println(ld2);
+        }
+
+        /*
+         * タイムゾーン
+         */
+        {
+
+            ZoneId zi = ZoneId.systemDefault();
+            ZoneId zi2 = ZoneId.of("UTC+02:00");
+            ZoneId zi3 = ZoneId.of("UTC-03:00");
+            ZoneId zi4 = ZoneId.of("UTC");
+            out.println(zi);
+            out.println(zi2);
+
+            ZonedDateTime  zdt = ZonedDateTime.now(zi);
+            ZonedDateTime  zdt2 = ZonedDateTime.now(zi2);
+            ZonedDateTime  zdt3 = ZonedDateTime.now(zi3);
+            ZonedDateTime  zdt4 = ZonedDateTime.now(zi4);
+
+            out.println(zdt);
+            out.println(zdt2);
+            out.println(zdt3);
+            out.println(zdt4);
+            //ゾーン情報が含まれているので、LONGのフォーマッタが使用できる
+            out.println(zdt.format(dtf6));
+            out.println(zdt4.format(dtf6));
+        }
+
+        /*
+         * 時差
+         */
+        {
+            OffsetDateTime odt = OffsetDateTime.now();
+            out.println(odt);
+            OffsetDateTime odt2 = OffsetDateTime.of(LocalDateTime.now(),ZoneOffset.of("+01:00"));
+            out.println(odt2);
+            //ローカルの時刻を固定し、時差だけ調整
+            out.println(odt2.atZoneSimilarLocal(ZoneId.systemDefault()));
+            //指定された時差を考慮して、ローカルの時刻を調整
+            out.println(odt2.atZoneSameInstant(ZoneId.systemDefault()));
+
+        }
+        /**
+         * 時間間隔
+         */
+        {
+            LocalDate start = LocalDate.of(2011, 4, 1);
+            LocalDate end = LocalDate.now();
+            Period p = Period.between(start, end);
+            out.println(p);
+        }
+        /*
+         * インスタント
+         */
+        {
+            Instant it1 = Instant.now();
+            Instant it2 = Instant.MIN;
+            out.println(it1);
+            out.println(it2);
+            it1.plus(0,ChronoUnit.FOREVER);
+
+        }
+
+    }
+
+}
